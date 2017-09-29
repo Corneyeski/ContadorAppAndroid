66e03624498af73d28fb6faec75c46add158561f
Alan <avallve@opentrends.net>
2017-09-29 09:35:29 +0200

    Initial commit

---
+++ app/src/main/java/com/example/avallve/cuentaapp/MainActivity.java
@@ -0,0 +1,49 @@
+package com.example.avallve.cuentaapp;
+
+import android.app.Activity;
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.TextView;
+
+public class MainActivity extends Activity {
+
+    int cuenta = 0;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+       /* TextView miTexto = new TextView(this);
+
+        miTexto.setText("WAZAAAAAh");
+
+        setContentView(miTexto);*/
+
+        //TextView contador = (TextView) R.id.contador;
+    }
+
+    protected void setValue(){
+
+        TextView contador = (TextView) findViewById(R.id.contador);
+
+        contador.setText("Contador: " + cuenta);
+    }
+
+    protected void increment(View vista){
+        cuenta++;
+        setValue();
+    }
+
+    protected void decrease(View vista){
+        cuenta--;
+        setValue();
+    }
+
+    protected void reset(View vista){
+        cuenta = 0;
+        setValue();
+    }
+
+}
